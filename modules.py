d17de5ce2943e51432c12ffc33dfe957e01c4ba7
Annie Maslan <amaslan@berkeley.edu>
2017-12-09 18:13:11 -0800

    add docstrings to modules functions

--- modules.py
+++ modules.py
@@ -2,16 +2,39 @@ import MySQLdb
 import seaborn as sns
 import pandas as pd
 
-# connect to MySQL database called p3
 def dbConnect():
+    """
+    connect to MySQL database called p3
+
+    Parameters
+    ----------
+    None
+
+    Return
+    ------
+    connection object from MySQLdb.connect()
+
+    """
     dsn_database = 'p3'
     dsn_uid = 'root'
     dsn_pwd = ''
     conn = MySQLdb.connect(user=dsn_uid, passwd=dsn_pwd, db=dsn_database)
     return conn
 
-# convert database table into dataframe
 def dbTableToDataFrame(conn, table):
+    """
+    convert table from p3 database into a dataframe
+
+    Parameters
+    ----------
+    1. connection object
+    2. table name to select from in database p3
+
+    Return
+    ------
+    dataframe with contents from table name passed
+    
+    """
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM ' + table)
     result = []
