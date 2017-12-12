75d47ff80916c16fae53ead959494a7ea3757328
mitch <mitchell.negus.57@gmail.com>
2017-12-11 18:51:14 -0800

    added class with most of the id tools as methods

--- modules.py
+++ modules.py
@@ -48,14 +48,76 @@ class SchoolID:
     def __init__(self):
         pass
 
-    def delete_stems(self,institutions,i):
+    def identify_school(self,institutions,i):
+        '''
+        Process a school name to give a more unique identifier.
+
+        Replace common stems with just the stem, delete others, and replace
+        keywords and nicknames with a common title.
+
+        Parameters
+        ----------
+        institutions : Series
+            A series of institution names, not identified.
+        i : int
+            The index of the school to be identified in institutions
+        '''
+        institutions[i] = self._delete_stems(institutions[i])
+        institutions[i] = self._reduce_to_stems(institutions[i])
+        institutions[i] = self._replace_by_keyword(institutions[i])
+        institutions[i] = self._replace_by_nickname(institutions[i])
+
+    def _delete_stems(self,institution):
         '''Delete some common superfluous words in institution names.'''
         del_stems = ['univ','school','law','business']
         del_word_inds = []
-        for word_i in range(len(institutions[i])):
+        for word_i in range(len(institution)):
             for del_stem in del_stems:
-                if institutions[i][word_i] == del_stem:
+                if institution[word_i][:len(del_stem)] == del_stem:
                     del_word_inds.append(word_i)
         for word_i in del_word_inds[::-1]:
-            del(institutions[i][word_i])
-        return
+            del(institution[word_i])
+        return institution
+
+    def _reduce_to_stems(self,institution):
+        '''Replace words with their stem to increase names in common.'''
+        stems = ['poly','tec','cal','inst','agri','coll']
+        for word_i in range(len(institution)):
+                for stem in stems:
+                    if institution[word_i][:len(stem)]==stem:
+                        institution[word_i]=stem
+        return institution
+
+    def _replace_by_keyword(self,institution):
+        '''Identify a school by matching a keyword.'''
+        keywords=['wharton','yale','harvard','mit','henley','berkeley','gurion',
+                  'duke','babson','bentley','canberra','boulder','stanford',
+                  'northwestern','usc','ucla','nyu','bowling green',
+                  'baldwin wallace','boston coll','cal culinary',
+                  'virginia commonwealth','virginia poly','stony brook',
+                  'georgia tec','coll park']
+        for name in keywords:
+            match_counter = 0
+            names = name.split()
+            for word in names:
+                if word in institution:
+                    match_counter += 1
+                if match_counter == len(names):
+                    institutions = names
+        return institution
+
+    def _replace_by_nickname(self,institution):
+        '''Identify a school when its nickname is given.'''
+        nicknames={'cal':'berkeley','pennsylvania':'penn','wharton':'penn',
+                   'virginia poly':'virginia tec','chicago booth':'chicago',
+                   'massachusetts inst tec':'mit','coll park':'maryland'}
+        for name in nicknames:
+            match_counter = 0
+            names = name.split()
+            for word in names:
+                if word in institution:
+                    match_counter +=1
+                if match_counter == len(institution) and match_counter == len(names):
+                    institution = [nicknames[name]]
+                    return institution
+        return institution
