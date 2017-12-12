539eea6932bebc0c3e414169480e7d15888ebcee
mitch <mitchell.negus.57@gmail.com>
2017-12-12 00:47:29 -0800

    added tests for SchoolTools class

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from modules import *
+import pandas as pd
 import numpy.testing as npt
 
 def test_connection_and_dataframe_cols():
@@ -35,14 +36,48 @@ def test_connection_and_dataframe_shape():
     obj_shape = (462651, 40)
     assert df.shape == obj_shape
 
+def test_SchoolID_remove_stopword():
+    institutions = pd.Series(['harvard university', 'the university of oregon'])
+    SD = SchoolTools()
+    ids = SD.remove_stopwords(institutions)
+    key = pd.Series(['harvard university','university oregon'])
+    assert ids.equals(key)
+
+def test_SchoolID_remove_punctuation():
+    institutions = pd.Series(['university of california, berkeley','harvard.'])
+    SD = SchoolTools()
+    ids = SD.remove_punctuation(institutions)
+    key = pd.Series(['university of california  berkeley', 'harvard'])
+    assert ids.equals(key)
+
 def test_SchoolID_delete_stems():
-    pass
+    institutions = pd.Series([['university','california'],['harvard','law']])
+    SD = SchoolTools()
+    for i in range(len(institutions)):
+        institutions[i] = SD._delete_stems(institutions[i])
+    key = pd.Series([['california'],['harvard']])
+    assert institutions.equals(key)
 
 def test_SchoolID_reduce_to_stems():
-    pass
+    institutions = pd.Series([['university','california'],['virginia','tech']])
+    SD = SchoolTools()
+    for i in range(len(institutions)):
+        institutions[i] = SD._reduce_to_stems(institutions[i])
+    key = pd.Series([['university','cal'],['virginia','tec']])
+    assert institutions.equals(key)
 
 def test_SchoolID_replace_by_keyword():
-    pass
+    institutions = pd.Series([['upenn','wharton'],['harvard','law']])
+    SD = SchoolTools()
+    for i in range(len(institutions)):
+        institutions[i] = SD._replace_by_keyword(institutions[i])
+    key = pd.Series([['wharton'],['harvard']])
+    assert institutions.equals(key)
 
 def test_SchoolID_replace_by_nickname():
-    pass
+    institutions = pd.Series([['cal'],['massachusetts','inst','tec']])
+    SD = SchoolTools()
+    for i in range(len(institutions)):
+        institutions[i] = SD._replace_by_nickname(institutions[i])
+    key = pd.Series([['berkeley'],['mit']])
+    assert institutions.equals(key)
